0f41ecb16edcb4a98f8f5964f1ac40200f77f12d
martinindra <martin.indra19@gmail.com>
2021-01-09 10:03:40 +0700

    splash screen and assets
    update splash screen pesan keselamatan dan assets

--- lib/Global/AssetsRelated/AssetsConstant.dart
+++ lib/Global/AssetsRelated/AssetsConstant.dart
@@ -3,6 +3,12 @@ class Assets {
   static const String login_logo = "assets/login/login_logo.png";
   static const String pesan_keselamatan_1 =
       "assets/pesan_keselamatan/pesan_keselamatan_1.png";
+  static const String pesan_keselamatan_2 =
+      "assets/pesan_keselamatan/pesan_keselamatan_2.png";
+  static const String pesan_keselamatan_3 =
+      "assets/pesan_keselamatan/pesan_keselamatan_3.png";
+  static const String pesan_keselamatan_4 =
+      "assets/pesan_keselamatan/pesan_keselamatan_4.png";
   static const String admin_icon = "assets/icons/admin_icon.png";
   static const String engineer_icon = "assets/icons/engineer_icon.png";
   static const String gl_icon = "assets/icons/gl_icon.png";
