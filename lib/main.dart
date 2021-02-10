dea246f06568bfc4c4ed54e30d23fd8e06223d7c
Muhammad Fahmi Hidayat <36060021+fahmih6@users.noreply.github.com>
2021-02-10 10:47:24 +0700

    PB-12 : Fix GL Order List Overflow. (#13)

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:rxdart/subjects.dart';
